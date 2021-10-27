2e239ad09762409d7e3dfb72da83e45580bd1a4a
skfy <shiro218266@gmail.com>
2021-10-27 11:51:32 +0000

    second commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: 'homes#top'
+  resources :books, only: [:new, :create, :index, :show, :destroy]
+  get 'about' => 'about#index'
 end
