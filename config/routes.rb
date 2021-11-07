0b3b0c6f4d6ed1f392ad10f3101b5716f3f336d1
skfy <shiro218266@gmail.com>
2021-11-07 12:06:56 +0000

    eighth commit

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy]
-  get 'about' => 'about#index'
+  get 'home/about' => 'homes#about'
   resources :users, only: [:show, :index, :edit, :update]
 end
